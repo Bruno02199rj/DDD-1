091c86c53659fd6c81f2ebe09510b5677c56825e
bruno.ferreira <bruno.ferreira@ascsolutions.com.br>
2025-05-23 18:53:01 -0300

    init

---
+++ ClassLibrary1/Entidades/Base/EntidadeBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Entidades.Base
+{
+    public class EntidadeBase 
+    {
+
+        public EntidadeBase()
+        {
+            Status = true;
+        }
+        public int Id { get; set; }
+        public DateTime DataInclusao { get; set; }
+        public DateTime? DataAlteracao { get; set; }
+        public bool Status { get; set; }
+    }
+}
